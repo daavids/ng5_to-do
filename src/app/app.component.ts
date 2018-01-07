3694cb49c2e5e9164547194327c16cbbbbbca3a7
Davids Gabalins <davidsgabalins@gmail.com>
2018-01-07 14:17:03 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<html stuff>`
   styleUrls: ['./app.component.scss']
+  // styles: [` css{} `]
 })
 export class AppComponent {
   title = 'app';
